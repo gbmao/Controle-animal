7a0b293386b82ec4c46ad0e22fe1b510227e8aca
gbmao <gabriel.machado.oli@gmail.com>
2025-10-30 19:33:00 -0300

    feat(model) Create abstract Animal and hierarchy with Cat

---
+++ controle-animalAPI/controle-animal/src/main/java/com/projeto/controleanimal/model/Animal.java
@@ -0,0 +1,32 @@
+package com.projeto.controleanimal.model;
+
+public abstract class Animal {
+    private String name;
+    private int age;
+
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // setters devem ser chamados apenas por  funçoes especificas
+    //ex crie uma funcao changeName()
+    void setName(String name) {
+        this.name = name;
+    }
+
+    //eventualmente criar um envelhecimento automatico baseado na data de nascimento do animal
+    // funcao birthday() que alem de alterar a idade daria um aviso de aniversario
+    void setAge(int age) {
+        this.age = age;
+    }
+}
--- controle-animalAPI/controle-animal/src/main/java/com/projeto/controleanimal/model/Cat.java
+++ controle-animalAPI/controle-animal/src/main/java/com/projeto/controleanimal/model/Cat.java
@@ -1,35 +1,17 @@
 package com.projeto.controleanimal.model;
 
-public class Cat {
-    private String name;
-    private int age;
+public class Cat extends Animal {
 
     public Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public String toString() {
         return "Cat{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
                 '}';
     }
 
