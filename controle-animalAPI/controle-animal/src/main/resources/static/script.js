da0f7d02d9825c3e76dba0343aa154e263509bc5
deboradevsouza <souza.deboravieira@gmail.com>
2025-11-02 15:21:53 -0300

    feat(ui): Update navigation link and implement API fetching for results display

--- controle-animalAPI/controle-animal/src/main/resources/static/script.js
+++ controle-animalAPI/controle-animal/src/main/resources/static/script.js
@@ -19,3 +19,37 @@ function changeTheme() {
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+const linkListar = document.getElementById("listarLink");
+const resultado = document.getElementById("resultado");
+
+// Define a URL da sua API
+const API_URL = "http://localhost:8080/api/all";
+
+// Função que busca a API
+async function buscarApi() {
+    try {
+    const resposta = await fetch(API_URL);
+    if (!resposta.ok) {
+        throw new Error("Erro ao buscar a API");
+    }
+
+    const dados = await resposta.json();
+
+    
+    resultado.innerHTML = `
+        <h2>Resultados:</h2>
+        <ul>
+        ${dados.map(item => `<li>${item.name}</li>`).join("")}
+        </ul>
+        `;
+    } catch (erro) {
+        resultado.innerHTML = `<p style="color:red;">${erro.message}</p>`;
+    }
+    }
+
+    // Quando clicar no link, cancela o comportamento padrão e chama a função
+    linkListar.addEventListener("click", (evento) => {
+      evento.preventDefault(); // impede o salto da âncora
+    buscarApi();
+    });
