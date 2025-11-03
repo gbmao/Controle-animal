ace7dc79939971ae0b80ffe4b091c60da5049b62
gbmao <gabriel.machado.oli@gmail.com>
2025-11-03 11:07:13 -0300

    feat:Create POST create Db.class export list as .txt

--- controle-animalAPI/controle-animal/src/main/java/com/projeto/controleanimal/service/AnimalService.java
+++ controle-animalAPI/controle-animal/src/main/java/com/projeto/controleanimal/service/AnimalService.java
@@ -1,5 +1,6 @@
 package com.projeto.controleanimal.service;
 
+import com.projeto.controleanimal.database.Db;
 import com.projeto.controleanimal.model.Animal;
 import com.projeto.controleanimal.model.Cat;
 import org.springframework.stereotype.Service;
@@ -9,15 +10,15 @@ import java.util.*;
 @Service
 public class AnimalService {
 
-    private Map<String,Animal> animals = new LinkedHashMap<>();
+    private Map<String, Animal> animals = new LinkedHashMap<>(Db.loadList());
 
     public AnimalService() {
 
         //---------------------------------------------------
         //criado para testes
-        animals.put("boris",new Cat("Boris", 5));
-        animals.put("jorge",new Cat("Jorge", 3));
-
+//        animals.put("boris",new Cat("Boris", 7));
+//        animals.put("ivaldo", new Cat("Ivaldo", 1));
+//        Db.saveList(animals);
     }
 
     public Map<String, Animal> getAnimals() {
@@ -30,11 +31,18 @@ public class AnimalService {
     }
 
     /**
-     *  Retorna NULL caso o nome não exista!(
+     * Retorna NULL caso o nome não exista!(
+     *
      * @param name
      * @return O animal
      */
     public Animal getAnimal(String name) {
         return animals.getOrDefault(name, null); //TODO lançar uma excessao em caso de null
+    }
+
+    public Animal addAnimal(Cat cat) {
+        animals.put(cat.getName().toLowerCase(), cat);
+        Db.saveList(animals);
+        return cat;
     }
 }
