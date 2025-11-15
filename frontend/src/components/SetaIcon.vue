3844a9b6058d473754f7c9e11a7e2008c093d3f7
deboradevsouza <souza.deboravieira@gmail.com>
2025-11-15 10:40:14 -0300

    Add SetaIcon components and expand to see info behaviour

---
+++ frontend/src/components/SetaIcon.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.01599 1.01599L6.09599 6.09599L11.176 1.01599" stroke="#7A9590" stroke-width="2.032" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
