2f49da13a86c1750b2e662d0cf7de35f0bdffea8
gbmao <gabriel.machado.oli@gmail.com>
2025-11-28 17:04:31 -0300

    add allowCredentials

--- backend/src/main/java/com/projeto/controleanimal/config/CorsConfig.java
+++ backend/src/main/java/com/projeto/controleanimal/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173", "https://controle-animal-production.up.railway.app",
                         "https://patrulha-felina.netlify.app","http://localhost:8080","http://localhost:8888" ) // Liste todas as origens permitidas
                 .allowedMethods("*") // Permite todos os métodos (GET, POST, OPTIONS, etc.)
-                .allowedHeaders("*"); // ESSENCIAL: Permite cabeçalhos customizados como 'x-api-key'
+                .allowedHeaders("*")
+                .allowCredentials(true); // ESSENCIAL: Permite cabeçalhos customizados como 'x-api-key'
     }
 }
