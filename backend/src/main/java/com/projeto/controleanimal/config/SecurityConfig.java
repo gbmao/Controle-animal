10fdebe1080c3b6867dd9aba7ed44769259b8021
gbmao <gabriel.machado.oli@gmail.com>
2025-11-29 13:59:24 -0300

    change CorsConfig

--- backend/src/main/java/com/projeto/controleanimal/config/SecurityConfig.java
+++ backend/src/main/java/com/projeto/controleanimal/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors( cors -> {})
+                .cors( withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**","/images/**").authenticated()
                         .anyRequest().permitAll()
